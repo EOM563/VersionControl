8b1f634544cfc1d78f3a7d9c77847e24e518d228
EOM563 <kissgy97@gmail.com>
2020-10-11 16:21:54 +0200

    4

--- week5/week5/Form1.cs
+++ week5/week5/Form1.cs
@@ -12,9 +12,15 @@ namespace week5
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
+
+
     }
 }
