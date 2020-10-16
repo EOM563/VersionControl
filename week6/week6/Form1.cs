19c8422c91cc1b51965f3eb8ef594939668e9370
EOM563 <kissgy97@gmail.com>
2020-10-16 23:17:20 +0200

    4

--- week6/week6/Form1.cs
+++ week6/week6/Form1.cs
@@ -1,17 +1,20 @@
 ﻿using System.Windows.Forms;
 using week6.MnbServiceReference;
+using week6.Entities;
+using System.ComponentModel;
 
 namespace week6
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
-
+            dataGridView1.DataSource = Rates;
             harmadik();
         }
-
+                
         private void harmadik()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
