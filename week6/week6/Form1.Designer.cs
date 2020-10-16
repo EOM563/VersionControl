3df81d902e7c377b0a4f10340476c98af280e43e
EOM563 <kissgy97@gmail.com>
2020-10-16 23:25:48 +0200

    5

--- week6/week6/Form1.Designer.cs
+++ week6/week6/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(13, 13);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(623, 425);
+            this.dataGridView1.Size = new System.Drawing.Size(314, 425);
             this.dataGridView1.TabIndex = 0;
             // 
             // Form1
--- week6/week6/Form1.cs
+++ week6/week6/Form1.cs
@@ -2,21 +2,25 @@
 using week6.MnbServiceReference;
 using week6.Entities;
 using System.ComponentModel;
+using System.Xml;
+using System;
 
 namespace week6
 {
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        
         public Form1()
         {
             InitializeComponent();
             dataGridView1.DataSource = Rates;
-            harmadik();
+            harmadik_otodik();
         }
                 
-        private void harmadik()
+        private void harmadik_otodik()
         {
+            //harmadik
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
@@ -28,11 +32,32 @@ namespace week6
 
             var response = mnbService.GetExchangeRates(request);
 
-
             var result = response.GetExchangeRatesResult;
-        }
 
-        
+            //otodik
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {                
+                var rate = new RateData();
+                Rates.Add(rate);
+
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+      
 
     }
 }
