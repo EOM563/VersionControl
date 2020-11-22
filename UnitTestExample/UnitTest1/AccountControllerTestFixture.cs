79d546cc401595d631ea197925b40ad086061d5e
EOM563 <kissgy97@gmail.com>
2020-11-22 19:00:56 +0100

    5

--- UnitTestExample/UnitTest1/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTest1/AccountControllerTestFixture.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using UnitTestExample.Controllers;
 
 namespace UnitTest1
 {
@@ -12,7 +13,14 @@ namespace UnitTest1
         [Test]
         public void TestValidateEmail(string email, bool expectedResult)
         {
+            // Arrange
+            var accountController = new AccountController();
 
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
